2bec018c7298749947dea92d18ceed07c43f2003
kannangr21 <kannanravi2105@gmail.com>
2022-12-30 00:47:39 +0530

    Changed Async await

--- app.js
+++ app.js
@@ -14,26 +14,22 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY.replace(/\\n/g, '\n');
 
 const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
 
-const appendSpreadsheet = async (row) => {
+const appendSpreadsheet = (row) => {
     console.log("Function Called!");
   try {
-    await doc.useServiceAccountAuth({
+    doc.useServiceAccountAuth({
       client_email: CLIENT_EMAIL,
       private_key: PRIVATE_KEY,
-    });
-
-    console.log("Service Account Authenticated");
-
-    doc.loadInfo().then(() => {
-        console.log("Loaded Doc info");
-        const sheet = doc.sheetsById[SHEET_ID];
-        const result = sheet.addRow(row).then(()=>{
-            console.log("Row written");
+    }).then(() => {
+        console.log("Service Account Authenticated");
+        doc.loadInfo().then(() => {
+            console.log("Loaded Doc info");
+            const sheet = doc.sheetsById[SHEET_ID];
+            const result = sheet.addRow(row).then(()=>{
+                console.log("Row written");
+            });
         });
-
     });
-
-    
   } catch (e) {
     console.error('Error: ', e);
   }
